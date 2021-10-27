7d448ea48f825637e21373ab6b25a689beab0880
slyce <d.a.mccoy@email.msmary.edu>
2021-10-27 14:50:22 -0400

    pushing logout html with some basic navigation links

--- app/routes.py
+++ app/routes.py
@@ -11,6 +11,16 @@ def start():
     return render_template('start.html', title='Start')
 
 
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    form = SignUp()  # instantiate object
+    if form.validate_on_submit():
+        flash('Email: {} | User ID: {} | Password: {}' .format(form.email.data, form.username.data, form.password.data))
+        # flash to display messages for prototype, driver
+        return redirect(url_for('home'))  # navigation when complete
+    return render_template('signup.html', title='Sign Up', form=form)  # pass form
+
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     form = Login()  # instantiate object
@@ -20,15 +30,10 @@ def login():
         return redirect(url_for('home'))  # navigation when complete
     return render_template('login.html', title='Log In', form=form)  # pass form
 
+@app.route('/logout')
+def logout():
+    return render_template('logout.html', title='Log Out')
 
-@app.route('/signup', methods=['GET', 'POST'])
-def signup():
-    form = SignUp()  # instantiate object
-    if form.validate_on_submit():
-        flash('Email: {} | User ID: {} | Password: {}' .format(form.email.data, form.username.data, form.password.data))
-        # flash to display messages for prototype, driver
-        return redirect(url_for('home'))  # navigation when complete
-    return render_template('signup.html', title='Sign Up', form=form)  # pass form
 
 
 @app.route('/home')
