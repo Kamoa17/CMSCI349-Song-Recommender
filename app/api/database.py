653fd088af116960cfd5a52fb48ac6d6fda21290
collinsnji <collins.geek@gmail.com>
2021-10-29 14:44:13 -0400

    Add project scafolding

---
+++ app/api/database.py
@@ -0,0 +1,57 @@
+from google.cloud import datastore
+import json
+
+datastore_client = datastore.Client()
+
+
+def add_user(user_info: dict):
+    """Adds a new user to th users table
+
+    Args:
+        user_info (dict): A key, value mapping of the user attributes
+    """
+    entity = datastore.Entity(key=datastore_client.key("users"))
+    entity.update(user_info)
+    datastore_client.put(entity)
+
+
+def get_user(user_id: int):
+    """Get the user info
+
+    Args:
+        user_id (int): The unique identifier of the user
+
+    Returns:
+        List[<datastore.Entity>]: The user info
+    """
+    query = datastore_client.query(kind="users")
+    query = query.add_filter("id", "=", user_id)
+    user = list(query.fetch())
+    return user
+
+
+def add_song_metadata(song_info: dict):
+    """Add a new song metadata
+
+    Args:
+        song_info (dict): Song metadata information
+    """
+    entity = datastore.Entity(key=datastore_client.key("song-metadata"))
+    entity.update(song_info)
+    datastore_client.put(entity)
+
+
+def get_song_metadata(song_id: int):
+    """Gets a song metadata
+
+    Args:
+        song_id (int): The unique ID of the song
+
+    Returns:
+        List[<datastore.Entity>]: The user info
+    """
+    query = datastore_client.query(kind="song-metadata")
+    song_key = datastore_client.key("song-metadata", song_id)
+    query = query.add_filter("__key__", "=", song_key)
+    results = list(query.fetch())
+    return json.dumps(dict(results[0]))
--- app/routes.py
+++ app/routes.py
@@ -1,41 +1,72 @@
 from app import app
-from flask import render_template, flash, redirect, url_for
+from flask import render_template, flash, redirect, url_for, request, jsonify
 from app.forms import Login, SignUp
+from app.api.database import get_song_metadata, add_song_metadata, add_user
+from markupsafe import escape
 
 
-@app.route('/')
-
-
-@app.route('/start')
+@app.route("/")
+@app.route("/start")
 def start():
-    return render_template('start.html', title='Start')
+    return render_template("start.html", title="Start")
 
 
-@app.route('/signup', methods=['GET', 'POST'])
+@app.route("/signup", methods=["GET", "POST"])
 def signup():
     form = SignUp()  # instantiate object
     if form.validate_on_submit():
-        flash('Email: {} | User ID: {} | Password: {}' .format(form.email.data, form.username.data, form.password.data))
+        flash(
+            "Email: {} | User ID: {} | Password: {}".format(
+                form.email.data, form.username.data, form.password.data
+            )
+        )
         # flash to display messages for prototype, driver
-        return redirect(url_for('home'))  # navigation when complete
-    return render_template('signup.html', title='Sign Up', form=form)  # pass form
+        return redirect(url_for("home"))  # navigation when complete
+    return render_template("signup.html", title="Sign Up", form=form)  # pass form
 
 
-@app.route('/login', methods=['GET', 'POST'])
+@app.route("/login", methods=["GET", "POST"])
 def login():
     form = Login()  # instantiate object
     if form.validate_on_submit():
-        flash('User ID: {} | Password: {}'.format(form.username.data, form.password.data))
+        flash(
+            "User ID: {} | Password: {}".format(form.username.data, form.password.data)
+        )
         # flash to display messages for prototype, driver
-        return redirect(url_for('home'))  # navigation when complete
-    return render_template('login.html', title='Log In', form=form)  # pass form
+        return redirect(url_for("home"))  # navigation when complete
+    return render_template("login.html", title="Log In", form=form)  # pass form
 
-@app.route('/logout')
+
+@app.route("/logout")
 def logout():
-    return render_template('logout.html', title='Log Out')
+    return render_template("logout.html", title="Log Out")
 
 
-
-@app.route('/home')
+@app.route("/home")
 def home():
-    return render_template('home.html', title='Home')
+    return render_template("home.html", title="Home")
+
+
+# API Routes
+@app.route("/api/song/metadata/<int:song_id>", methods=["GET"])
+def get_song_meta(song_id):
+    song_meta = get_song_metadata(song_id=song_id)
+    return song_meta
+
+
+@app.route("/api/song/metadata/add", methods=["POST"])
+def add_song_meta():
+    song_data = request.json
+    add_song_metadata(song_info=song_data)
+
+    # @TODO add error handling
+    return "done"
+
+
+@app.route("/api/users/add", methods=["POST"])
+def add_new_user():
+    user_data = request.json
+    add_user(user_info=user_data)
+
+    # @TODO add error handling
+    return "done"
